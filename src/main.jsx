7c7e0e997e9a5f8cc862c18113f635bd4d8cd53d
Kelvin Ng <hoishing@gmail.com>
2023-12-15 00:49:38 +0800

    basic react structure

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+// create main component and render
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
